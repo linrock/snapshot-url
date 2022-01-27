c24841d1d3bf9835a390d54c0e088a3c7757c17b
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-27 10:57:11 -0500

    Print usage if required args not specified

--- bin/snapshot-url.js
+++ bin/snapshot-url.js
@@ -7,6 +7,11 @@ const { writeFile } = require("../src/file_utils");
 const url = process.argv[2];
 const outFile = process.argv[3];
 
+if (!url || !outFile) {
+  console.log('Usage: snapshot-url <url> <out_file>');
+  process.exit(1);
+}
+
 let finalUrl;
 if (!url.startsWith('http')) {
   finalUrl = `file://${path.resolve(url)}`;
